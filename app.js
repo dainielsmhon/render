f9fd75bb3256640dc0261eb0dadd87abe2784958
daniel simhon <danielsimhon931@gmail.com>
2024-11-19 23:22:30 +0200

    added Msg new

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port=process.env.PORT||3030;
 app.use(morgan('dev'));
 //הגדרנו נקודת קצה בשיטת נט שמחזירה הודעה של כל מוצר שנשלח
 app.get('/products',(req,res)=>{
-    res.status(200).json({Msg:"All Product"})
+    res.status(200).json({Msg:"All Product new"})
 });
 
 app.listen(port,()=>{console.log('Server Started')});
