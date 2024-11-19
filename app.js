b9af038570469e5e1dc6c19a9993c525600e9938
daniel simhon <danielsimhon931@gmail.com>
2024-11-19 23:03:50 +0200

    added morgan to project

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 require('dotenv').config();
 const express=require('express');
 const app= express();
+const morgan=require('morgan');
 const port=process.env.PORT||3030;
+
+app.use(morgan('dev'));
 //הגדרנו נקודת קצה בשיטת נט שמחזירה הודעה של כל מוצר שנשלח
 app.get('/products',(req,res)=>{
     res.status(200).json({Msg:"All Product"})
